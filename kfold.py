45ce99a5f346d0a80704ad466dcefeedea8da589
Zubair Qazi <zubair.qazi.5b@gmail.com>
2021-03-15 15:14:08 -0700

    Add new testing data

--- kfold.py
+++ kfold.py
@@ -3,12 +3,15 @@ import numpy as np
 import pandas as pd
 
 
-def cross_validation(feature, current_features, idx):
-    data = pd.read_csv('data/CS170_SMALLtestdata__70.txt', delim_whitespace = True, header = None)
+def cross_validation(complete_data, current_features, feature_to_add):
+
+    # remove columns we are not interested in
+    data = complete_data[[0] + list(current_features) + [feature_to_add]]
     correctly_classified = 0
+    num_rows = len(data)
 
     # FIXME: iterate over number of rows
-    for i in range(10):
+    for i in range(num_rows):
         objects_to_classify = data.iloc[i, 1:]
         label = data.iloc[i, 0]
 
@@ -16,7 +19,7 @@ def cross_validation(feature, current_features, idx):
         nn_label = 0.0
 
         # FIXME: iterate over number of rows
-        for j in range(10):
+        for j in range(num_rows):
             if i != j:
                 # print('Ask if {} is nearest neighbors with {}'.format(i+1, j+1))
                 distance = np.sqrt(sum(pow(objects_to_classify - data.iloc[j, 1:], 2)))
@@ -32,9 +35,9 @@ def cross_validation(feature, current_features, idx):
         print('Its nearest neighbor is {} which is in class {}'.format(nn_location, nn_label))
 
     # FIXME: Update 10 to number of rows
-    accuracy = correctly_classified / 10
+    accuracy = correctly_classified / num_rows
     return accuracy
 
 
 if __name__ == '__main__':
-    cross_validation([], [], 0)
+    cross_validation(pd.DataFrame(), [], 0)
--- search.py
+++ search.py
@@ -1,4 +1,4 @@
-import pandas as np
+import pandas as pd
 import random
 
 from kfold import cross_validation
@@ -6,35 +6,38 @@ from kfold import cross_validation
 
 # TODO: Implement backward search
 
-def forward_search(features):
+def forward_search(data):
     # set of features selected
     current_features = set()
 
+    num_cols = len(data.columns)
     # iterate through each level
-    for i in range(len(features)):
-
+    for i in range(num_cols - 1):
         # store feature to add and best accuracy so far
-        feature_to_add = 0
+        feature_to_add = None
         best_accuracy = 0
 
         print('On level {} of the search tree'.format(i + 1))
         # iterate through each feature
-        for j in range(len(features)):
+        for j in range(num_cols - 1):
             if j + 1 not in current_features:
                 print('\tConsidering adding feature {}'.format(j + 1))
-                accuracy = cross_validation(features, current_features, j + 1)
+                accuracy = cross_validation(data, current_features, j + 1)
 
                 # update best accuracy if improvement exists
                 if accuracy > best_accuracy:
                     best_accuracy = accuracy
+                    print('Updated Accuracy:', best_accuracy)
                     feature_to_add = j + 1
 
         current_features.add(feature_to_add)
         print('Feature {} added on level {}\n'.format(feature_to_add, i + 1))
 
+    print('Best Accuracy:', best_accuracy)
+
     return 0
 
 
 if __name__ == '__main__':
-    inp = [1, 2, 3, 4]
-    forward_search(inp)
+    input_data = pd.read_csv('data/CS170_small_special_testdata__95.txt', delim_whitespace=True, header=None)
+    forward_search(input_data)
