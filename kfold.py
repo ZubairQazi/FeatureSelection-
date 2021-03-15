47a2cb39e5e8dfbcf32b1a0eca3557e25c55ca56
Zubair Qazi <zubair.qazi.5b@gmail.com>
2021-03-15 16:09:09 -0700

    Replace pandas dataframe with numpy array

--- kfold.py
+++ kfold.py
@@ -1,38 +1,44 @@
 import random
+import sys
 import numpy as np
-import pandas as pd
-
+from copy import deepcopy
 
 def cross_validation(complete_data, current_features, feature_to_add):
 
-    # remove columns we are not interested in
-    data = complete_data[[0] + list(current_features) + [feature_to_add]]
+    # TODO: remove columns we are not interested in
+    data = deepcopy(complete_data)
+    for i in range(1, complete_data.shape[1]):
+        if i not in current_features and i != feature_to_add:
+            data[:, i] = 0
+
+    # np.set_printoptions(precision=2, threshold=sys.maxsize)
+    # print(data)
     correctly_classified = 0
-    num_rows = len(data)
+    num_rows = data.shape[0]
 
     # FIXME: iterate over number of rows
     for i in range(num_rows):
-        objects_to_classify = data.iloc[i, 1:]
-        label = data.iloc[i, 0]
+        objects_to_classify = data[i, 1:]
+        label = data[i, 0]
 
-        nn_distance = nn_location = 1000000.0
+        nn_distance = nn_location = 1000.0
         nn_label = 0.0
 
         # FIXME: iterate over number of rows
         for j in range(num_rows):
             if i != j:
                 # print('Ask if {} is nearest neighbors with {}'.format(i+1, j+1))
-                distance = np.sqrt(sum(pow(objects_to_classify - data.iloc[j, 1:], 2)))
+                distance = np.sqrt(sum(pow(objects_to_classify - data[j, 1:], 2)))
                 if distance < nn_distance:
                     nn_distance = distance
                     nn_location = j
-                    nn_label = data.iloc[nn_location, 0]
+                    nn_label = data[nn_location, 0]
 
         if label == nn_label:
             correctly_classified += 1
 
-        print('Object {} is in class {}'.format(i+1, label))
-        print('Its nearest neighbor is {} which is in class {}'.format(nn_location, nn_label))
+        # print('Object {} is in class {}'.format(i+1, label))
+        # print('Its nearest neighbor is {} which is in class {}'.format(nn_location, nn_label))
 
     # FIXME: Update 10 to number of rows
     accuracy = correctly_classified / num_rows
@@ -40,4 +46,4 @@ def cross_validation(complete_data, current_features, feature_to_add):
 
 
 if __name__ == '__main__':
-    cross_validation(pd.DataFrame(), [], 0)
+    cross_validation([], [], 0)
--- search.py
+++ search.py
@@ -10,16 +10,16 @@ def forward_search(data):
     # set of features selected
     current_features = set()
 
-    num_cols = len(data.columns)
+    num_cols = data.shape[1]
     # iterate through each level
-    for i in range(num_cols - 1):
+    for i in range(num_cols - 2):
         # store feature to add and best accuracy so far
         feature_to_add = None
         best_accuracy = 0
 
         print('On level {} of the search tree'.format(i + 1))
         # iterate through each feature
-        for j in range(num_cols - 1):
+        for j in range(num_cols - 2):
             if j + 1 not in current_features:
                 print('\tConsidering adding feature {}'.format(j + 1))
                 accuracy = cross_validation(data, current_features, j + 1)
@@ -39,5 +39,5 @@ def forward_search(data):
 
 
 if __name__ == '__main__':
-    input_data = pd.read_csv('data/CS170_small_special_testdata__95.txt', delim_whitespace=True, header=None)
+    input_data = pd.read_csv('data/CS170_small_special_testdata__95.txt', delim_whitespace=True, header=None).values
     forward_search(input_data)
