3db527cadebafceb2524ff3ab85a85c0f1c76b61
Zubair Qazi <zubair.qazi.5b@gmail.com>
2021-03-17 12:00:30 -0700

    Add backward elim graph

--- graph.py
+++ graph.py
@@ -17,12 +17,27 @@ def graph_small():
     plt.savefig('graphs/{}.png'.format(file_name))
 
 
-def graph_large():
+def graph_large_forward():
     file_name = input('Enter a file name to use for the graph image: ')
 
     x = ['{14}', '{14, 20}', '{14, 20, 19}', '{14, ..., 46}', '{Omitted', 'For', 'Space}',
          '{14, ..., 12}', '{All Features}']
     y = [0.802, 0.978, 0.95, 0.91, 0, 0, 0, 0.612, 0.574]
+
+    fig, ax = plt.subplots(figsize=(20, 8))
+    ax.bar(x, y, tick_label=x)
+    ax.set_xlabel('Current Feature Set')
+    ax.set_ylabel('Accuracy')
+    ax.set_title('Feature Selection: Large Dataset')
+    plt.savefig('graphs/{}.png'.format(file_name))
+
+
+def graph_large_backward():
+    file_name = input('Enter a file name to use for the graph image: ')
+
+    x = ['{All Features}', '{AF} - {95}', '{AF} - {95} - {51}', '{AF} - ... - {96}', '{Omitted', 'For', 'Space}',
+         '{9, 14, 86, 100}', '{9, 14, 86}', '{9, 14}', '{14}']
+    y = [0.576, 0.612, 0.63, 0.646, 0, 0, 0, 0.816, 0.828, 0.826, 0.802]
 
     fig, ax = plt.subplots(figsize=(20, 8))
     ax.bar(x, y, tick_label=x)
@@ -48,4 +63,4 @@ def graph_feature_selection():
 
 
 if __name__ == '__main__':
-    graph_feature_selection()
+    graph_large_backward()
